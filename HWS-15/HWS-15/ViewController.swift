fcb3025df2317a13424e60b799f073b76206b0b7
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-06-02 00:32:21 +0200

    Pingwin?

---
+++ HWS-15/HWS-15/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  HWS-15
+//
+//  Created by Szymon Blaszczynski on 01/06/16.
+//  Copyright © 2016 Szymon Blaszczynski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tap: UIButton!
+    @IBAction func tapped(sender: UIButton) {
+        tap.hidden = true
+        
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: { [unowned self] in
+                                    switch self.currentAnimation {
+                                    case 0:
+                                        self.imageView.transform = CGAffineTransformMakeScale(1.2, 1.2)
+                                    case 1:
+                                        self.imageView.transform = CGAffineTransformIdentity
+                                    case 2:
+                                        self.imageView.transform = CGAffineTransformMakeTranslation(+384, -210)
+                                    case 3:
+                                        self.imageView.transform = CGAffineTransformIdentity
+                                    case 4:
+                                        self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+                                    case 5:
+                                        self.imageView.transform = CGAffineTransformIdentity
+                                    case 6:
+                                        self.imageView.alpha = 0.1
+                                        self.imageView.backgroundColor = UIColor.greenColor()
+                                    case 7:
+                                        self.imageView.alpha = 1
+                                        self.imageView.backgroundColor = UIColor.clearColor()
+                                    default:
+                                        break
+                                    }
+        }) { [unowned self] (finished: Bool) in
+            self.tap.hidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imageView = UIImageView(image: UIImage(named: "rainbow"))
+        imageView.center = CGPoint(x: 0, y: 384)
+        view.addSubview(imageView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
